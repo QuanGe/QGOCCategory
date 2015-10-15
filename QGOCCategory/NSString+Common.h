096005847f2d582b3de158eec309e55a041af9db
QuanGe <zhang_ru_quan@163.com>
2015-10-15 21:51:51 +0800

    NSString (Common)
    NSString (Common)

---
+++ QGOCCategory/NSString+Common.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Common.h
+//  QGOCCategory
+//
+//  Created by 张如泉 on 15/10/15.
+//  Copyright © 2015年 QuanGe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Common)
+
+@end
---
+++ QGOCCategory/NSString+Common.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Common.m
+//  QGOCCategory
+//
+//  Created by 张如泉 on 15/10/15.
+//  Copyright © 2015年 QuanGe. All rights reserved.
+//
+
+#import "NSString+Common.h"
+
+@implementation NSString (Common)
+
+@end
